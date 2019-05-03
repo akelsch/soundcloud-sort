266aa2e2019a0f272194be42b965ede445a07878
Arthur Kelsch <akelsch@users.noreply.github.com>
2019-05-03 21:18:43 +0200

    Parse track likes

--- soundcloud-sort.user.js
+++ soundcloud-sort.user.js
@@ -15,12 +15,33 @@
         return numberText.replace(/,/g, "");
     }
 
+    /**
+     * Although plays are displayed formatted, e.g. "11.2M", their exact
+     * integer value is given in the title attribute, e.g. "11,266,303 plays".
+     */
     function getTrackPlays(soundListItem) {
-        let plays = soundListItem.querySelector(".soundStats > li").getAttribute("title");
-        return parseInt(removeThousandsSeperator(plays));
+        let rawPlays = soundListItem.querySelector(".soundStats > li").getAttribute("title");
+        let parsedPlays = parseInt(removeThousandsSeperator(rawPlays));
+
+        return parsedPlays;
+    }
+
+    /**
+     * There are no exact values for likes available so we have to convert
+     * the formatted strings like "119K", "41.1K" and "9,766" manually.
+     */
+    function getTrackLikes(soundListItem) {
+        let rawLikes = soundListItem.querySelector(".sc-button-like").textContent;
+        let parsedLikes = parseFloat(removeThousandsSeperator(rawLikes));
+
+        if (rawLikes.endsWith("K"))
+            parsedLikes *= 1000;
+
+        return parsedLikes;
     }
 
     const compareTracksByPlays = (t1, t2) => getTrackPlays(t1) > getTrackPlays(t2) ? -1 : 1;
+    const compareTracksByLikes = (t1, t2) => getTrackLikes(t1) > getTrackLikes(t2) ? -1 : 1;
 
     function sortTracks(compareFunction) {
         let parent = document.querySelector(".soundList > ul");
@@ -38,7 +59,7 @@
     let sortByLikesButton = document.createElement("button");
     sortByLikesButton.className = "sc-button sc-button-medium sc-button-responsive";
     sortByLikesButton.textContent = "Sort by likes"
-    sortByLikesButton.onclick = () => sortTracks(compareTracksByPlays);
+    sortByLikesButton.onclick = () => sortTracks(compareTracksByLikes);
 
     let buttons = document.querySelector(".userInfoBar__buttons > .sc-button-group");
     buttons.prepend(sortByPlaysButton);
